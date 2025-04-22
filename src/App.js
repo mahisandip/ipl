02c6c11605c2c8611516869779825275dfa4730d
Sandeepc287 <sandeep.chandrasekharan@izeno.com>
2025-04-22 12:59:27 +0800

    ipl poll app initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'; // Add this import
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import MyPolls from './pages/MyPolls';
